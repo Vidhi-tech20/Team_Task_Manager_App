ef61b3e5e532620d05ba2e90d2301c8976dd635c
Vidhi-tech20 <vidhi1064@gmail.com>
2026-05-07 21:13:03 +0530

    fix the backend link in the frontend

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
   baseURL:
     import.meta.env.VITE_API_URL ||
-    "http://localhost:5000/api", // Fallback to localhost if VITE_API_URL is not set
+    "https://teamtaskmanagerapp-production-46ae.up.railway.app/api", // Fallback to localhost if VITE_API_URL is not set
 
   headers: {
     "Content-Type": "application/json",
