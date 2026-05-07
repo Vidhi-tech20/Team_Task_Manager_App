f9f83696587a69f19705f4a4b2753c0c87dbb1df
Vidhi-tech20 <vidhi1064@gmail.com>
2026-05-07 21:39:21 +0530

    fix the socket.io file

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 // Apna deployed backend URL yahan daal dena
-const rawUrl = import.meta.env.VITE_API_URL || "https://teamtaskmanagerapp-production-46ae.up.railway.app";
+const rawUrl = import.meta.env.VITE_API_URL || "wss://teamtaskmanagerapp-production-46ae.up.railway.app";
 const BACKEND_URL = rawUrl.replace(/\/api\/?$/, "");
 
 export const socket = io(BACKEND_URL, {
